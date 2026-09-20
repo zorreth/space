a14d625b6aba6fd6f47ebede48684d73465832dc
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-20 20:36:17 +0500

    feat(backend): add created_at to the users table

--- backend/migrations/20260830115317_init.up.sql
+++ backend/migrations/20260830115317_init.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR NOT NULL
+    name VARCHAR NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE chunks (
@@ -9,6 +10,6 @@ CREATE TABLE chunks (
     colors_data BYTEA NOT NULL,
     author_ids_data BYTEA NOT NULL,
     timestamps_data BYTEA NOT NULL,
-    updated_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     PRIMARY KEY (chunk_x, chunk_y)
 );
